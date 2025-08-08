9e54bacaa45125045e52acd40db4bcbd6c157a8a
Divyanka Barnwal <barnwaldivyanka@gmail.com>
2025-08-08 16:03:08 +0530

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "divyanka123"; // Replace with your actual password
+$password = " "; // Replace with your actual password
 $database = "hiremap";
 
 // Create connection
@@ -12,3 +12,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
